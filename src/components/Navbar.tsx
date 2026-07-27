6235b5212de89f8928457be8cefbd19dd480600e
Ryan <ryanwbadenhorst@gmail.com>
2026-07-27 12:40:00 +0200

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <nav className="flex flex-row border rounded-lg mb-5">
             <a href="">Ryan</a>
-            <div className="ml-auto flex flex-row gap-20">
+            <div className="ml-auto flex flex-row gap-32">
                 <div className="flex flex-row gap-4">
                     <a href="">Projects</a>
                     <a href="">About</a>
