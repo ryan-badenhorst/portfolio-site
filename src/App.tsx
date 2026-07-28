53772a191443f5297f1f360efc8422ae4aba5aff
Ryan <ryanwbadenhorst@gmail.com>
2026-07-28 13:27:03 +0200

    Updated styling across components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import FeaturedProjects from "./components/FeaturedProjects"
 
 export default function App() {
     return (
         <>
-            <Navbar></Navbar>
-            <Hero></Hero>
-            <FeaturedProjects></FeaturedProjects>
+            <Navbar />
+            <Hero />
         </>
     )
 }
--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,10 +1,14 @@
 export default function FeaturedProjects() {
     return (
+        <>
+        <h1>Featured Projects</h1>
         <div className="flex flex-col gap-10 mb-5">
-            <div className="project 1 flex flex-row gap-8 h-[60vh]">
+            <h2>Pokemon Nuzlocke Tracker</h2>
+            <div className="project 1 flex flex-row gap-8 h-[80vh]">
                 <div className="border w-1/2"></div>
                 <div className="border w-1/2"></div>
             </div>
         </div>
+        </>
     )
 }
