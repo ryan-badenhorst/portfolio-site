ae4cde93bd32e6482302ca79401fabec99cdbf98
Ryan <ryanwbadenhorst@gmail.com>
2026-07-28 20:35:07 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,22 +2,25 @@ export default function Hero() {
     return (
         <div className="flex flex-row gap-10 mb-25 h-screen">
             <div className="info border p-5 w-1/2">
-                <p>Hi, I am Ryan Badenhorst</p>
-                <p>I am a Front-end web developer located in Durban, South Africa</p>
+                <p>Hi, I'm</p>
+                <p>Ryan Badenhorst</p>
+                <p>Front-End Web Developer</p>
+                <p>Durban, South Africa</p>
                 <p>I enjoy building responsive, accessible web applications with React and TypeScript.</p>
+                <p>Currently focused on creating polished user interfaces while continually expanding my front-end development skills.</p>
 
                 <p>
                     Primary stack
                     React · TypeScript · JavaScript · HTML · CSS
                 </p>
 
+                <button>View my resume</button>
+
                 <div>
                     <p>Github Icon</p>
                     <p>LinkedIn Icon</p>
                     <p>Email Icon</p>
                 </div>
-
-                <button>View my resume</button>
             </div>
             <div className="picture border w-1/2"></div>
         </div>
