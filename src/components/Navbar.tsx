aeebb411cad57ed210277ea6af93dcd98cb98f13
Ryan <ryanwbadenhorst@gmail.com>
2026-07-29 13:41:57 +0200

    Updated Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
+import topLogo from "../images/top-logo.jpg";
+
 export default function Navbar() {
     return (
-        <nav className="flex flex-row border rounded-lg mb-5">
-            <a href="">Ryan</a>
+        <nav className="flex flex-row border mb-5 text-xl">
+            <a href="#hero" className="pl-3 flex flex-row items-center">
+                <img src={topLogo} alt="Top Logo" className="w-16 h-16"/>
+                <p className="font-bold p-5">Ryan Badenhorst</p>
+            </a>
+
             <div className="flex flex-row gap-4 ml-auto">
-                <a href="">Projects</a>
-                <a href="">About</a>
-                <a href="">Skills</a>
-                <a href="">Contact</a>
+                <a href="" className="navbar-link">Projects</a>
+                <a href="" className="navbar-link">About</a>
+                <a href="" className="navbar-link">Skills</a>
+                <a href="" className="navbar-link">Contact</a>
             </div>
         </nav>
     )
