96024251d372d320084f5b48647a15fb253af589
Ryan <ryanwbadenhorst@gmail.com>
2026-08-01 19:04:55 +0200

    Added a second project to FeaturedProjects.tsx

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,10 +1,11 @@
 import ProjectCard from "./ProjectCard";
 
 import PokemonNuzlockeTracker from "../images/pokemon-nuzlocke-tracker.jpg"
+import WeatherDashboard from "../images/weather-daashboard.png"
 
 export default function FeaturedProjects() {
     return (
-        <section id="projects" className="mb-20">
+        <section id="projects" className="mb-10">
             <h1 className="section-headings mb-3">
                 Featured Projects
             </h1>
@@ -22,12 +23,27 @@ export default function FeaturedProjects() {
                 image={PokemonNuzlockeTracker}
                 imageAlt="Pokemon Nuzlocke Tracker pages"
                 about="A React web application that helps players manage Pokémon Nuzlocke playthroughs by tracking encounters, routes, team members, boxed Pokémon, and overall progress throughout a run."
-                technologies={["React", "React Router", "TypeScript", "Tailwind CSS"]}
+                technologies={["React", "React Router", "TypeScript", "Tailwind CSS", "PokéAPI"]}
                 highlights={["Track encounters by route", "Manage active team and boxed Pokémon", "Search and browse the Pokédex", "Automatically save progress using Local Storage"]}
                 learned="Building this application taught me how to structure larger React applications using reusable components, organize state across multiple pages, and think more carefully about designing interfaces that remain intuitive as new features are added."
                 githubUrl="https://github.com/ryan-badenhorst/pokemon-nuzlocke-tracker"
                 liveDemoUrl="pokemon-nuzlocke-tracker.netlify.app/"
             />
+
+            <ProjectCard 
+                title="Weather Dashboard"
+                image={WeatherDashboard}
+                imageAlt="Weather Dashboard Web App"
+
+                about="A React web application that displays current weather conditions and forecasts for locations around the world using data from a third-party weather API. The application provides an intuitive interface for searching and viewing weather information."
+                technologies={["React", "TypeScript", "Tailwind CSS", "Open-Meteo API"]}
+                highlights={["Search weather by city", "Display current weather conditions", "View extended weather forecasts", "Loading states while fetching data", "Responsive interface across devices"]}
+                learned="Building this project gave me practical experience working with external APIs, managing asynchronous data in React, and designing components that update dynamically as new data is received."
+
+                githubUrl="https://github.com/ryan-badenhorst/react-typescript-weather-dashboard"
+                liveDemoUrl="https://react-typescript-weather-dashboard.netlify.app/"
+            />
+
         </section>
     )
 }
