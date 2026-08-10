b3f8d6129e6ce2c0e4109ef4a5aab51efe3ec25e
Ryan <ryanwbadenhorst@gmail.com>
2026-08-10 12:45:00 +0200

    Added and updated Skills.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import FeaturedProjects from "./components/FeaturedProjects"
 import About from "./components/About"
+import Skills from "./components/Skills"
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <Hero />
             <FeaturedProjects />
             <About />
+            <Skills />
         </>
     )
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,5 @@
+export default function Skills() {
+    return (
+        <h1>This is the Skills and Tools section</h1>
+    )
+}
