73905d229a05031a4b8551106bddc857cc5891f9
Ryan <ryanwbadenhorst@gmail.com>
2026-09-01 21:16:38 +0200

    Updated Navbar responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import topLogo from "../images/top-logo.jpg";
 
 export default function Navbar() {
     return (
-        <nav className="flex flex-row text-xl bg-slate-50 max-[673px]:flex-col max-[900px]:border-b-indigo-300" id="nav">
+        <nav className="flex flex-row text-xl bg-slate-50 max-[673px]:flex-col shadow-[0_2px_4px_-2px_rgba(0,0,0,0.25)]" id="nav">
             <a href="#hero" className="pl-3 flex flex-row items-center">
                 <img src={topLogo} alt="Top Logo" className="w-16 h-16 max-[673px]:w-12 max-[673px]:h-12"/>
                 <p className="font-bold p-5 max-[673px]:p-3">Ryan Badenhorst</p>
